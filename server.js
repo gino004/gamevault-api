f5fdd169410bcdfc7ea575a8e857070787662e42
gino004 <gino2137@hotmail.com>
2026-07-25 17:31:12 -0500

    Initial project setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 8080;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to GameVault API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
